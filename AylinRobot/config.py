641494145ed28f1ebce157e1ab65d3def60ae03c
Lerro <makar1212tg@gmail.com>
2025-04-25 15:12:55 +0400

    Update config.py

--- AylinRobot/config.py
+++ AylinRobot/config.py
@@ -1,33 +1,26 @@
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
-# Reponu Satan Kodların Götürən Kimliyindən Aslı Olmayaraq Peysərdi
 
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
 
 import os
 
 class Config:
 
-   API_ID = int(os.getenv("API_ID", "8953338"))
-   API_HASH = os.getenv("API_HASH", "fe21f223cb02d8f7c1cbda651f553a45")
+   API_ID = int(os.getenv("API_ID", "12345789"))
+   API_HASH = os.getenv("API_HASH", "12345789")
    BOT_TOKEN = os.getenv("BOT_TOKEN", "6")
-   BOT_USERNAME = os.environ.get("BOT_USERNAME", "dddd")
-   BOT_NAME = os.environ.get("BOT_NAME", "Fatime")   
-   OWNER_ID = int(os.environ.get("OWNER_ID","6799203666"))
-   OWNER_NAME = os.environ.get("OWNER_NAME", "huseynh") 
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "XezerGameRobot")
+   BOT_NAME = os.environ.get("BOT_NAME", "XəzərGame")   
+   OWNER_ID = int(os.environ.get("OWNER_ID","7218327869"))
+   OWNER_NAME = os.environ.get("OWNER_NAME", "LerroAndMee") 
    GONDERME_TURU = bool(os.environ.get("GONDERME_TURU", "False"))
    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://Huseyn:Huseyn@cluster0.zelpu0o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002209683682"))
-   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "fffff")
+   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "XezerBotlar")
    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1002209683682"))
    BAN_GROUP = int(os.environ.get("BAN_GROUP", "-1002209683682"))
    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "05a017d5-e3e5-424d-941f-3e60645e3141")
    ALIVE_NAME = os.environ.get("ALIVE_NAME", "HUSEYN")
-   CHANNEL = os.environ.get("CHANNEL", "huseynh")
-   SUPPORT = os.environ.get("SUPPORT", "huseynh")
+   CHANNEL = os.environ.get("CHANNEL", "XezerBotlar")
+   SUPPORT = os.environ.get("SUPPORT", "Xezer_Support")
    ALIVE_IMG = os.environ.get("ALIVE_IMG", "https://telegra.ph/file/f6c186e3c581a223856c4.mp4") 
-   START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/b2c2ed59a89933a384ae3.jpg")
+   START_IMG = os.environ.get("START_IMG", "https://web.telegram.org/023cf4cd-5a3a-42ef-b48d-bf4e22133d91")
    COMMAND_PREFIXES = list(os.environ.get("COMMAND_PREFIXES", "/ ! .").split())
